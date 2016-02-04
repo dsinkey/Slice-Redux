fa07fa3dd4180037123e12d1d46174cef5a1d403
Dustin <dustinsinkey1@gmail.com>
2016-02-03 18:14:28 -0800

    Added react to modules and created Header component

---
+++ public/js/components/Header.react.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Header = React.createClass({
+  render: function (){
+    return {
+      <header>
+        <div id="logo"></div>
+      </header>
+      <nav>
+        <div id="nav-links"></div>
+      </nav>
+    };
+  }
+});
